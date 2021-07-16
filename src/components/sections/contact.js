e0d97a785ae59ed6a6dba72c2cd40fdfc5077eb8
Pustak Pathak <74170759+PustakP@users.noreply.github.com>
2021-07-17 00:46:54 +0530

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -57,11 +57,11 @@ const Contact = () => {
     <StyledContactSection id="contact" ref={revealContainer}>
       <h2 className="numbered-heading overline">What’s Next?</h2>
 
-      <h2 className="title">Get In Touch</h2>
+      <h2 className="title">MESSAGE ME</h2>
 
       <p>
-        Although I'm not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        I AM ALWAYS STARING FOR ATTENTION PLEASE CONTACT ME
+        THANK YOU AND HAVE A GOOD DAY
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
