9a58343427dc803e0466f1bc4ba4c4d6fab46582
Pustak Pathak <74170759+PustakP@users.noreply.github.com>
2021-07-16 21:15:59 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Pustak Pathak',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Pustak Pathak is a 16 year old teenager who likes to be delusional.',
+    siteUrl: 'https://pustak.tech', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@pustakalt',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'PustakPathak',
+        short_name: 'PustakPathak',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
